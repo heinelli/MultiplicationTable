46bce62d22cc597b026ba00af000d73ecf53c5d2
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-14 19:22:19 -0600

    Add header and option strict/explicit.

---
+++ MultiplicationTable/MultiplicationTable.vb
@@ -0,0 +1,16 @@
+﻿'Elliot Heiner
+'RCET0265
+'Spring 2021
+'Multiplication Table
+'https://github.com/heinelli/MultiplicationTable.git
+
+Option Strict On
+Option Explicit On
+
+Module MultiplicationTable
+
+    Sub Main()
+
+    End Sub
+
+End Module
