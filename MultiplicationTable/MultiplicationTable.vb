dfc528bdb7894cd8c609a07f0bb29b258a1ab58d
rosstimo <rosstimo@isu.edu>
2021-09-20 08:29:33 -0600

    see feedback comments

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -10,11 +10,11 @@ Option Explicit On
 Module MultiplicationTable
 
     Sub Main()
-
         Dim tableDimensions As Integer
         Dim iterationCount As Integer
         Dim wholeNumber As Boolean
 
+        'Prompt user input and prevent string input.
         Do While wholeNumber = False
             Try
                 Console.WriteLine("Please type a whole number.")
@@ -24,30 +24,28 @@ Module MultiplicationTable
                 wholeNumber = False
             End Try
         Loop
-        'Prompt user input and prevent string input.
 
         Console.WriteLine("Enjoy your " & tableDimensions & " X " & tableDimensions &
                                   " multiplication table")
+        'Number of rows is dictated by input. Function dictates length of rows.
         For i = 1 To tableDimensions
             iterationCount = i
-            Console.Write(RowCount(iterationCount, tableDimensions))
+            'Console.Write(RowCount(iterationCount, tableDimensions))
+            RowCount(iterationCount, tableDimensions)
             Console.WriteLine()
         Next
-        'Number of rows is dictated by input. Function dictates length of rows.
         Console.Read()
     End Sub
 
-    Function RowCount(ByVal iterationCount As Integer, ByVal tableDimensions As Integer) As String
-
+    Sub RowCount(ByVal iterationCount As Integer, ByVal tableDimensions As Integer)
         Dim stringConversion As String
 
+        'For loop within the function allows multiple integers in one row.
         For i = 1 To tableDimensions
             stringConversion = CStr(i * iterationCount)
             Console.Write(stringConversion.PadLeft(6))
         Next
-        Return Nothing
-
-    End Function
-    'For loop within the function allows multiple integers in one row.
+        'Return Nothing - Sub does not return anything - TJR
+    End Sub
 
 End Module
